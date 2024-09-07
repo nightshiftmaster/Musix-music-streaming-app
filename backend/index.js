9dbea7230b8296d906eb9a48f55a032407e799de
nightshiftmaster <nightshiftmaster@gmail.com>
2024-09-07 19:40:31 +0300

    server update

--- backend/index.js
+++ backend/index.js
@@ -14,9 +14,9 @@ mongoose.connect(process.env.MONGO);
 app.use(cors());
 app.use(bodyParser.json());
 
-// app.use("/api", require("./api"));
+app.use("/api", require("./api"));
 
-app.use("/", (req, res) => res.send("server is running"));
+// app.use("/", (req, res) => res.send("server is running"));
 
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "public", "index.html"));
